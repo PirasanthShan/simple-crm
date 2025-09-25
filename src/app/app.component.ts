4ceb15673ef6abfb62000d418226cbdae7e3df40
Pirasanth Shanmuganathan <pirasanth@live.de>
2025-09-25 22:53:56 +0200

    toggle menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';   
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIcon } from '@angular/material/icon'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatToolbar, MatToolbarRow],
+  imports: [CommonModule, 
+            RouterOutlet, 
+            MatToolbar, 
+            MatToolbarRow, 
+            MatSidenavModule,
+            MatIcon
+           ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'] 
 })
