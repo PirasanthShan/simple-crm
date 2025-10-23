b48c93ccce039fef8553bda505a96a77fe56816e
Pirasanth Shanmuganathan <pirasanth@live.de>
2025-10-23 23:39:26 +0200

    firebase implement user in screen

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,24 +1,53 @@
-import { Component, inject } from '@angular/core';
-import { MATERIAL_IMPORTS } from '../material.imports';          
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { MATERIAL_IMPORTS } from '../material.imports';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 
+import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { Subject, firstValueFrom } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+type UserDoc = User & { id: string };
+
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MATERIAL_IMPORTS],                                 
+  imports: [CommonModule, ...MATERIAL_IMPORTS],
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']                             
+  styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {
-  
+export class UserComponent implements OnDestroy {
   user = new User();
-  
-  
-  private dialog = inject(MatDialog);                             
+  allUsers: UserDoc[] = [];
 
-  openDialog() {
-    this.dialog.open(DialogAddUserComponent);
+  private dialog = inject(MatDialog);
+  private firestore = inject(Firestore);
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    const usersRef = collection(this.firestore, 'users');
+    collectionData(usersRef, { idField: 'id' })
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((users) => {
+        this.allUsers = users as UserDoc[];
+        console.log('Received changes from DB', this.allUsers);
+      });
+  }
+
+  async openDialog() {
+    const ref = this.dialog.open(DialogAddUserComponent);
+    const result = await firstValueFrom(ref.afterClosed());
+    if (!result) return;
+
+    const usersRef = collection(this.firestore, 'users');
+    await addDoc(usersRef, result);
+    console.log('User added:', result);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
